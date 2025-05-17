337e3c51527f535cdd1a613cbcba4dfe6b5806af
sahajp-11 <125601575+sahajp-11@users.noreply.github.com>
2025-05-17 13:16:54 -0400

    added

--- public/main.js
+++ public/main.js
@@ -25,7 +25,7 @@ form.addEventListener('submit',
     const lat = data.location.lat;
     const lon = data.location.lon;
 
-    await fetch('https://inst-377-final-project-ten.vercel.app/locations', {
+    await fetch('/locations', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
